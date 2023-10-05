fa53b1a6058e7b429e7dd51d31d12551e8447bfe
Berhanu Kebito <brhankbt63@gmail.com>
2023-10-05 22:37:40 +0300

    audio service configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:music_app/audio_helpers/page_manager.dart';
+import 'package:music_app/audio_helpers/service_locator.dart';
 import 'package:music_app/common/color_extension.dart';
 import 'package:music_app/view/splash_view.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized;
+  await setupServiceLocator();
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getIt<PageManager>().init();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    getIt<PageManager>().dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
